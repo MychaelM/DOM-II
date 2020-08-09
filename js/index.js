ee2b72aaa1f96112ba1881cf17198b226fe02e3d
Mychael M <mychaelbmenges@gmail.com>
2020-08-09 17:26:28 -0600

    added transform to images on mouseenter and mouseleave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+const images = document.querySelectorAll("img");
+
+images.forEach((event) => {
+  event.addEventListener("mouseenter", (e) => {
+    e.target.style.transform = "scale(1.5)";
+    e.target.style.transition = "transform 1s";
+})});
+images.forEach((event) => {
+  event.addEventListener("mouseleave", (e) => {
+    e.target.style.transform = "scale(1)";
+    e.target.style.transition = "transform 1s";
+})});
+
+console.log(images);
